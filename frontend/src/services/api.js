5bb8bf9ddecf42ed1c963b31f0784f1409c3b602
Lucia Predescu <luciana-579@hotmail.com>
2025-01-30 17:58:36 +0100

    changed api to js and the path to api.

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:8080', // Replace with your backend URL
+    baseURL: 'http://localhost:8000/api', // Replace with your backend URL
 });
 
 export const fetchFreezers = async () => {
     const response = await API.get('/freezers'); // Replace with your endpoint
     return response.data;
 };
+
 
