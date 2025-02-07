bebe44a6cac74f2248de3dab33b3b797c4b8fd92
Lucia Predescu <luciana-579@hotmail.com>
2025-02-07 21:50:34 +0100

    add 2 more localhost in config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Header from './components/Header.jsx';
 import Dashboard from './pages/Dashboard.jsx';
+import Home from "./pages/Home";
+import {BrowserRouter as Router,Route,Routes} from "react-router-dom";
 
 
 console.log('App.tsx: Rendering App component...');
--- frontend/src/components/HomeContent.jsx
+++ frontend/src/components/HomeContent.jsx
@@ -3,24 +3,17 @@ import React from "react";
 const HomeContent = () => {
     return (
         <div>
-            <h1>Welcome to the Freezer Alarm Management System</h1>
-
-                <h1>Welcome to the Freezer Alarm Management System</h1>
-                <p>
-                    This system helps monitor and manage freezer temperatures in real-time.
-                    Get alerts, view logs, and ensure optimal storage conditions.
-                </p>
-
-                <h2>Why Use This System?</h2>
+            <h2>Welcome to the Freezer Alarm Management System</h2>
+            <p>This system helps monitor and manage freezer temperatures in real-time.
+                Get alerts, view logs, and ensure optimal storage conditions.</p>
+            <h2>Why Use This System?</h2>
                 <ul>
                     <li>✔ Monitor freezer temperatures in real-time</li>
                     <li>✔ Receive instant alerts for temperature fluctuations</li>
                     <li>✔ Access detailed temperature logs and reports</li>
                 </ul>
-
-                <h2>How It Works</h2>
-                <p>
-                    Our system connects to sensors placed inside freezers. When temperature
+            <h2>How It Works</h2>
+                <p>Our system connects to sensors placed inside freezers. When temperature
                     changes beyond the safe range, alerts are triggered, and corrective actions
                     can be taken immediately.
                 </p>
