66f8f658b05d57d81d9bf8a7a67e73b34f624963
ValentinaSukonina <v.trana@icloud.com>
2025-02-11 21:32:45 +0100

    Simple Footer created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,8 @@ import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 import Header from './components/Header.jsx';
 import Home from "./pages/Home";
+import {BrowserRouter as Router,Route,Routes} from "react-router-dom";
 import Personal from "./pages/Personal";
-import Footer from "./components/Footer";
 
 
 console.log('App.tsx: Rendering App component...');
