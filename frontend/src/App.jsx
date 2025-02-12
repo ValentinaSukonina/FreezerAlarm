717eb44bca5e352113fd0c3b7efcc49041814821
ValentinaSukonina <v.trana@icloud.com>
2025-02-12 21:00:14 +0100

    Header and Footer moved outside Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,30 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+
 import Header from './components/Header.jsx';
 import Home from "./pages/Home";
+import Footer from "./components/Footer.jsx";
 import Personal from "./pages/Personal";
-import Footer from "./components/Footer";
-import CreateAccount from "./pages/CreateAccount";
-
 
 
 console.log('App.tsx: Rendering App component...');
 
 const App = () => {
     return (
-        <Router>
-            <Header />
-            <Routes>
-                <Route path="/" element={<Home/>} />
-                <Route path="/personal" element={<Personal/>} />
-                <Route path="/create-account" element={<CreateAccount />} />
-            </Routes>
-            <Footer/>
-        
-        </Router>
+        <div className="main-content">
+            <Router>
+                <Header/>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/personal" element={<Personal/>}/>
+                </Routes>
+                <Footer/>
+            </Router>
+        </div>
     );
 };
 
-export default App
+export default App;
 
 
 
