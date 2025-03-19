9e1da13b3262a2ae2c8fd88e67c7226ece26a236
Marcus <marcus.olsson98@gmail.com>
2025-03-19 20:45:38 +0100

    Add V3_Insert_initial_freezers.sql

---
+++ backend/src/main/resources/db/migration/V3_Insert_initial_freezers.sql
@@ -0,0 +1,11 @@
+INSERT INTO freezer (file, number, address, room, type) VALUES
+                                                            ('Storage A', '1001', '123 Lab Street', 'A1', '-80C'),
+                                                            ('Storage B', '1002', '123 Lab Street', 'A2', '-80C'),
+                                                            ('Storage C', '1003', '123 Lab Street', 'A3', '-80C'),
+                                                            ('Storage D', '1004', '123 Lab Street', 'A4', '-80C'),
+                                                            ('Storage E', '1005', '123 Lab Street', 'B1', '-80C'),
+                                                            ('Storage F', '1006', '123 Lab Street', 'B2', '-80C'),
+                                                            ('Storage G', '1007', '123 Lab Street', 'B3', '-80C'),
+                                                            ('Storage H', '1008', '123 Lab Street', 'B4', '-80C'),
+                                                            ('Storage I', '1009', '123 Lab Street', 'C1', '-80C'),
+                                                            ('Storage J', '1010', '123 Lab Street', 'C2', '-150C');
